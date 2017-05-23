d4e2d8fc3442336856a3c0394a52e5a549fe03bb
Alex <alexandrapop@op.pl>
2017-05-23 19:30:23 +0200

    installation process

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+  <p>it's your email {{email}}</p>
+  <p> and your address {{address.street}} {{address.city}} , {{address.region}} </p>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+name = 'Alex';
+email = 'alex@co.pl';
+address = {
+ street: 'Main12',
+ city: 'Posen',
+ region: 'GreatPoland'
+}}
