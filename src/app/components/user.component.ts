ad849971d673dad361f22b972c9e1b370a43be7a
Alex <alexandrapop@op.pl>
2017-05-24 10:27:01 +0200

    form, todolist

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -2,17 +2,35 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'user',
-  template: `<h1>Hello {{name}}</h1>
+  template: `<h1>{{name}}</h1>
   <p>it's your email {{email}}</p>
   <p> and your address {{address.street}} {{address.city}} , {{address.region}} </p>
   <button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies" : "Show Hobbies"}}</button>
   <div *ngIf="showHobbies">
   <h2>Hobbies</h2>
-  <ul>
-  <li *ngFor="let hobby of hobbies">{{hobby}}</li>
-  </ul>
+    <ul>
+      <li *ngFor="let hobby of hobbies; let i = index">{{hobby}} <button (click)="deleteHobby()">x</button>
+      </li>
+    </ul>
+    <form (submit)="addHobby(hobby.value)">
+    <label>Add Hobby:</label><br />
+    <input type="text" #hobby /><br />
+    </form>
   </div>
-
+  <hr />
+  <h4>Edit Data</h4>
+  <form>
+    <label>Name:</label><br />
+    <input type="text" name="name" [(ngModel)]="name" /><br />
+    <label>Email:</label><br />
+    <input type="text" name="email" [(ngModel)]="email" /><br />
+    <label>Street:</label><br />
+    <input type="text" name="address.street" [(ngModel)]="address.street" /><br />
+    <label>City:</label><br />
+    <input type="text" name="address.city" [(ngModel)]="address.city" /><br />
+    <label>Region:</label><br />
+    <input type="text" name="address.region" [(ngModel)]="address.region" /><br />
+  </form>
   `,
 })
 export class UserComponent {
@@ -40,6 +58,12 @@ export class UserComponent {
       this.showHobbies = true;
     }
 
+  }
+  addHobby(hobby){
+    this.hobbies.push(hobby);
+  }
+  deleteHobby(i){
+    this.hobbies.splice(i,1);
   }
 }
 interface address {
